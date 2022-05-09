c30eb39b161ee0242876febc7cecbd124a98facd
René Torres <7orresrene@users.noreply.github.com>
2022-05-08 20:28:58 -0400

    Fix scss

--- src/App.vue
+++ src/App.vue
@@ -21,34 +21,3 @@
 		},
 	}
 </script>
-<style lang="scss">
-	@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
-
-	* {
-		background: #fff;
-	}
-	.skip-to-main {
-		// background: $underline-pink;
-		background: #ed78b1;
-		color: #000;
-		height: 30px;
-		top: 0;
-		left: 0;
-		padding: 8px;
-		position: absolute;
-		transform: translateY(-90%);
-		transition: transform 0.3s;
-
-		&:visited {
-			color: #000;
-		}
-
-		&:focus {
-			top: 0;
-			transform: translateY(0%);
-		}
-	}
-
-	// #app {
-	// }
-</style>
