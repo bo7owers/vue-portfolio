9a5ce43cd85dc7dd34ca7737029046c2bbbb2510
bo7owers <rene7orres@gmail.com>
2022-06-17 22:30:51 -0400

    Start adding nested nav bar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
 
 const routes = [
-	{
-		path: '/',
-		name: 'Home',
-		component: Home,
-	},
-	{
-		path: '/about',
-		name: 'About',
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(/* webpackChunkName: "about" */ '../views/About.vue'),
-	},
-	{
-		path: '/portfolio',
-		name: 'Portfolio',
-		component: () => import('../views/Portfolio.vue'),
-	},
-	{
-		path: '/contact',
-		name: 'Contact',
-		component: () => import('../views/Contact.vue'),
-	},
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/portfolio",
+    name: "Portfolio",
+    component: () => import("../views/Portfolio.vue"),
+    children: [
+      { path: "coding", component: () => import("../views/ProCoding.vue") },
+      {
+        path: "photography",
+        component: () => import("../views/Photography.vue"),
+      },
+    ],
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("../views/Contact.vue"),
+  },
+];
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
-	routes,
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
