7544df99b9f4e5812c220b0419d4a660b672b414
bo7owers <7orres.rene@proton.me>
2022-11-13 22:17:05 -0500

    Delete css files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/sass/css/styles.css' //custom styles
+import './assets/sass/styles.scss' //custom styles
 
 const app = createApp(App)
 app.use(router)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,17 @@
     <h1>René Torres</h1>
     <p>
         I am a web developer who wanst to make the web accessible to everybody.
+        <a href="#">testing styles</a>
     </p>
     <div class="main-img"></div>
     <button>Hello there</button>
 </template>
+
+<style lang="scss" scoped>
+button {
+    width: 1rem;
+}
+</style>
 
 <!-- <script lang="ts">
 	const imgPath = ['../assets/img/memoji.png']
