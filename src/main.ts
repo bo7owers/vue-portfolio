85cb8a81ae6c1e52738d2d63919b07febcbbc225
bo7owers <7orres.rene@proton.me>
2022-11-27 18:08:36 -0500

    Install pinia with yarn

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
 import './assets/sass/styles.scss' //custom styles
-
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
