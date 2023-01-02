77ea99184809b76bfe2c83faff58146b5d5ceb20
bo7owers <7orres.rene@proton.me>
2023-01-02 08:53:42 -0500

    Remove unneeded button

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,28 +1,27 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
-import Button from "primevue/button";
+import { ref, onMounted } from 'vue'
 
-let count = ref<number>(0);
+let count = ref<number>(0)
 
 const increment = () => {
-  count.value++;
-};
+    count.value++
+}
 
 onMounted(() => {
-  console.log(`The initial count is ${count.value}`);
-});
+    console.log(`The initial count is ${count.value}`)
+})
 </script>
 
 <template>
-  <h1>Portfolio</h1>
-  <section id="profession">
-    <h2>Profesional Work</h2>
-    <!-- Todo: Links to CSPS work -->
-  </section>
-  <hr />
-  <section>
-    <h2>Hobbies</h2>
-    <!-- Todo: redirect to my github -->
-  </section>
-  <Button @click="increment">Count is {{ count }}</Button>
+    <h1>Portfolio</h1>
+    <section id="profession">
+        <h2>Profesional Work</h2>
+        <!-- Todo: Links to CSPS work -->
+    </section>
+    <hr />
+    <section>
+        <h2>Hobbies</h2>
+        <!-- Todo: redirect to my github -->
+    </section>
+    <button @click="increment">Count is {{ count }}</button>
 </template>
