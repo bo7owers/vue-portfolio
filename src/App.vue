c09ba1b618636d97b859c13e61c827adeedaf34b
bo7owers <7orres.rene@proton.me>
2023-01-02 09:35:28 -0500

    Add footer support

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,21 @@ import Navigation from './components/Navigation.vue'
             <!-- <div class="side-left">Aside goes here</div> -->
             <main
                 id="main_content"
-                class="content-area row-center row-flex"
+                class="content-area row-center row-flex row-flex-center"
                 role="main"
                 tabindex="-1"
             >
                 <router-view />
             </main>
+            <footer class="grid-footer row-flex">
+                Footer goes here
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Nostrum vitae laborum minima corporis sed ipsum odio natus
+                    sit itaque? Iure quo a voluptates ab inventore sint libero
+                    non! Eos, ad?
+                </p>
+            </footer>
         </div>
     </div>
 </template>
