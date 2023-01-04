a71d3754b9c46bd87c9004ad9d662748c37a9429
bo7owers <7orres.rene@proton.me>
2023-01-03 21:24:18 -0500

    Organize markup for a sticky footer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,9 @@ import Footer from './components/Footer.vue'
 </script>
 <template>
     <a href="#main_content" class="skip-to-main"> Skip to main content </a>
-    <div class="app">
-        <Navigation />
-        <div class="container">
+    <div class="container all-info">
+        <Navigation class="row-full" />
+        <div class="row-center">
             <main
                 id="main_content"
                 class="content-area row-center row-flex row-flex-center"
