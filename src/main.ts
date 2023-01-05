d1dcfd8981ea1ffc1ef270558add1bc9bbe68417
bo7owers <7orres.rene@proton.me>
2023-01-04 20:50:36 -0500

    Install tippy.js

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import VueTippy from 'vue-tippy'
 
 import './assets/sass/styles.scss' //custom styles
+import 'tippy.js/dist/tippy.css' // tooltip styles
+
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+app.use(VueTippy, {
+    directive: 'tippy', // => v-tippy
+    component: 'tippy', // => <tippy/>
+    componentSingleton: 'tippy-singleton', // => <tippy-singleton/>,
+    defaultProps: {
+        allowHTML: true,
+        delay: [150, 200],
+    },
+})
 app.mount('#app')
