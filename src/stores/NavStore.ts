972f394a57ccb49f5205462002669c796b714bb5
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-20 21:43:35 -0500

    Add methods to NavStore

--- src/stores/NavStore.ts
+++ src/stores/NavStore.ts
@@ -1,12 +1,34 @@
 import { defineStore } from 'pinia'
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 
 export const useNavStore = defineStore('navStore', () => {
     const isNavOpen = ref<Boolean>(false)
     const dropdownMenuActive = ref<Boolean>(false)
+
+    // methods
+    function closeIsNavOPen() {
+        setTimeout(() => {
+            dropdownMenuActive.value = false
+            isNavOpen.value = false
+        }, 400)
+    }
+
+    function toggleIsNavOpen() {
+        if (dropdownMenuActive.value === true) {
+            setTimeout(() => {
+                dropdownMenuActive.value = false
+                isNavOpen.value = false
+            }, 500)
+        } else {
+            dropdownMenuActive.value = true
+            isNavOpen.value = true
+        }
+    }
     return {
         // things to return
         isNavOpen,
         dropdownMenuActive,
+        toggleIsNavOpen,
+        closeIsNavOPen,
     }
 })
