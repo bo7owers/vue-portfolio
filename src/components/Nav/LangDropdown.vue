1e39699b090dab3349446272e7a1d8068afccb6a
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-03-18 21:59:57 -0400

    Make dopdown inputs links again lol

--- src/components/Nav/LangDropdown.vue
+++ src/components/Nav/LangDropdown.vue
@@ -20,11 +20,11 @@ const changeLang = (lang: string) => {
 </script>
 
 <template>
-    <button
+    <a
         href="#"
         class="link lang-dropdown"
-        @click="navStore.toggleIsNavOpen"
-        :class="dropdownMenuActive && 'active'"
+        @click.prevent="navStore.toggleIsNavOpen"
+        :class="dropdownMenuActive && 'router-link-active'"
         :title="title"
     >
         <svg
@@ -38,16 +38,21 @@ const changeLang = (lang: string) => {
                 d="M12.65 15.67c.14-.36.05-.77-.23-1.05l-2.09-2.06l.03-.03A17.52 17.52 0 0 0 14.07 6h1.94c.54 0 .99-.45.99-.99v-.02c0-.54-.45-.99-.99-.99H10V3c0-.55-.45-1-1-1s-1 .45-1 1v1H1.99c-.54 0-.99.45-.99.99c0 .55.45.99.99.99h10.18A15.66 15.66 0 0 1 9 11.35c-.81-.89-1.49-1.86-2.06-2.88A.885.885 0 0 0 6.16 8c-.69 0-1.13.75-.79 1.35c.63 1.13 1.4 2.21 2.3 3.21L3.3 16.87a.99.99 0 0 0 0 1.42c.39.39 1.02.39 1.42 0L9 14l2.02 2.02c.51.51 1.38.32 1.63-.35zM17.5 10c-.6 0-1.14.37-1.35.94l-3.67 9.8c-.24.61.22 1.26.87 1.26c.39 0 .74-.24.88-.61l.89-2.39h4.75l.9 2.39c.14.36.49.61.88.61c.65 0 1.11-.65.88-1.26l-3.67-9.8c-.22-.57-.76-.94-1.36-.94zm-1.62 7l1.62-4.33L19.12 17h-3.24z"
             />
         </svg>
-    </button>
+    </a>
     <ul
         class="dropdown-menu"
         v-show="dropdownMenuActive === true"
         :class="isNavOpen && 'opened'"
     >
         <li v-for="lang in availableLocales" :key="lang">
-            <button class="link" :class="lang" @click="changeLang(lang)">
+            <a
+                class="link"
+                :class="lang"
+                @click.prevent="changeLang(lang)"
+                href="#"
+            >
                 {{ lang }}
-            </button>
+            </a>
         </li>
     </ul>
 </template>
@@ -55,13 +60,13 @@ const changeLang = (lang: string) => {
 <style lang="scss" scoped>
 @use '../../assets/sass/vars/colors' as c;
 @use '../../assets/sass/base/extends';
+%btn-width {
+    width: calc(var(--ddm-width) - 2rem);
+}
 
 .lang-dropdown {
-    // color: #369 !important;
-    outline: 1px solid #000;
-    margin-block-end: 0;
+    margin-block-start: 0.25rem;
     background-color: c.$so-white;
-    border: none;
     cursor: pointer;
 }
 
@@ -77,13 +82,14 @@ const changeLang = (lang: string) => {
     width: var(--ddm-width);
     background-color: c.$so-white;
     li {
-        width: calc(var(--ddm-width) - 2rem);
+        @extend %btn-width;
     }
-    button.link {
+    .link {
         display: flex;
         justify-content: center;
+        @extend %btn-width;
+        height: 1.75rem;
         background-color: c.$so-white;
-        border: none;
         cursor: pointer;
         @extend %link-styles;
 
