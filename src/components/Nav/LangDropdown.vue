4aef58d0295fcad3476252cd31edb95dc9af2c79
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-03-18 22:17:50 -0400

    Add tooltip to dropdown trigger

--- src/components/Nav/LangDropdown.vue
+++ src/components/Nav/LangDropdown.vue
@@ -27,7 +27,7 @@ const changeLang = (lang: string) => {
         class="link lang-dropdown"
         @click.prevent="navStore.toggleIsNavOpen"
         :class="dropdownMenuActive && 'router-link-active'"
-        :title="title"
+        v-tippy="title"
     >
         <svg
             xmlns="http://www.w3.org/2000/svg"
