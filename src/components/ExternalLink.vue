82a0529fe6cc13c03feea6ee60cbf4b664a6b69f
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-04-01 20:45:28 -0400

    Add visited and hover same colours

--- src/components/ExternalLink.vue
+++ src/components/ExternalLink.vue
@@ -36,20 +36,11 @@ defineProps<{
         background-repeat: no-repeat;
     }
 
-    &:hover {
+    &:hover,
+    &:visited {
         color: c.$tertiary-color;
         &::after {
             content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="none" stroke="%23003cf0" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 6.52 5 7.08 5 8.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V14m1-5V4m0 0h-5m5 0l-7 7"%2F%3E%3C%2Fsvg%3E');
-        }
-    }
-    &:visited {
-        color: c.$visited;
-        &:hover {
-            color: c.$tertiary-color;
-        }
-        &::after {
-            content: '';
-            filter: grayscale(80%);
         }
     }
 }
