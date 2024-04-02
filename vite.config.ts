c0e5eb05446c7d865d1f055bc5eb699e438e3a18
bo7owers <74225602+bo7owers@users.noreply.github.com>
2024-04-01 20:40:54 -0400

    add postcss-nesting #81

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),  VueI18nPlugin({ /* options */ }),]
- 
+    plugins: [
+        vue(),
+        VueI18nPlugin({
+            /* options */
+        }),
+    ],
+    css: {
+        postcss: {
+            plugins: [postcssNesting],
+        },
+    },
 })
