6603e5c2abd3ab028fc0df41c61a61cb8762f38b
bo7owers <74225602+bo7owers@users.noreply.github.com>
2024-04-03 19:38:59 -0400

    rm prefers-color media query

--- src/components/ExternalLink.vue
+++ src/components/ExternalLink.vue
@@ -6,26 +6,23 @@ const { t } = useI18n()
 defineProps<{
     href: string
     description: string
-    pageName: string
+    pageName?: string
 }>()
 </script>
 <template>
-    <a
-        :href="href"
-        target="_blank"
-        rel="noopener noreferrer"
-        v-tippy="t('openIn')"
-        class="external"
-        >{{ description }}
+    <a :href="href" target="_blank" rel="noopener noreferrer" v-tippy="t('openIn')" class="external">{{ description }}
     </a>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/sass/vars/colors' as c;
+
 .external {
     position: relative;
     color: c.$brand-blue;
+    margin-inline-end: 21px;
     transition: all 500 cubic-bezier(0.165, 0.84, 0.44, 1);
+
     &::after {
         content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="none" stroke="%23285b86" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 6.52 5 7.08 5 8.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V14m1-5V4m0 0h-5m5 0l-7 7"%2F%3E%3C%2Fsvg%3E');
         width: 2em;
@@ -43,6 +40,7 @@ defineProps<{
         }
     }
 
+
     &:hover,
     &:visited {
         color: c.$tertiary-color;
@@ -50,17 +48,28 @@ defineProps<{
         &::after {
             content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="none" stroke="%23003cf0" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 6.52 5 7.08 5 8.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V14m1-5V4m0 0h-5m5 0l-7 7"%2F%3E%3C%2Fsvg%3E');
         }
-        @media (prefers-color-scheme: dark) {
-            color: c.$dropdown-shadow1;
-            &::after {
-                content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="none" stroke="%23e8e8e8" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 6.52 5 7.08 5 8.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V14m1-5V4m0 0h-5m5 0l-7 7"%2F%3E%3C%2Fsvg%3E');
-            }
-        }
+
     }
-    @media (prefers-color-scheme: dark) {
+
+}
+
+html.dark-theme {
+    .external {
         color: c.$brand-pink;
+        transition: all 500 ease-in;
+
         &::after {
             content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="none" stroke="%23ed78b1" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 6.52 5 7.08 5 8.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V14m1-5V4m0 0h-5m5 0l-7 7"%2F%3E%3C%2Fsvg%3E');
+        }
+
+        &:hover,
+        &:visited {
+            color: c.$dropdown-shadow1;
+
+            &::after {
+                content: url('data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="24" height="24" viewBox="0 0 24 24"%3E%3Cpath fill="none" stroke="%23e8e8e8" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 5H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 6.52 5 7.08 5 8.2v7.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.987.218-2.105V14m1-5V4m0 0h-5m5 0l-7 7"%2F%3E%3C%2Fsvg%3E');
+
+            }
         }
     }
 }
