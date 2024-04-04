a39454343bdd7d63332edd9d93ec637e267f51df
bo7owers <74225602+bo7owers@users.noreply.github.com>
2024-04-03 20:10:04 -0400

    start adding content to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,76 @@
 <script lang="ts" setup>
-import ExternalLink from '../components/ExternalLink.vue'
 import { useI18n } from 'vue-i18n'
-interface person {
-    name: String
-    lName: String
-    age?: Number
-}
+import ExternalLink from '../components/ExternalLink.vue'
 
-const rene: person = {
-    name: 'Rene',
-    lName: 'Torres',
-    age: 29,
-}
-
-const { t } = useI18n()
+const { t, locale } = useI18n()
 </script>
 
 <template>
     <h1>{{ t('homeHeading') }}</h1>
 
-    <h2>{{ rene.name }} {{ rene.lName }}</h2>
-    <p>
-        I am a web developer who wanst to make the web accessible to everybody.
-        <ExternalLink
-            pageName="7orresrene"
-            href="https://vue.7orresrene.com"
-            description="My website's beta version"
-        />
-    </p>
+    <div v-if="locale === 'en'">
+        <p>I am a web developer with experience on
+            <ExternalLink href="https://www.goskills.com/Resources/Adult-learning-theory"
+                description="adult learning" /> and
+            <ExternalLink href="https://dictionary.cambridge.org/dictionary/english/e-learning"
+                description="e-learning" />.
+        </p>
 
-    <div class="main-img"></div>
-    <input type="text" name="test" id="test" />
-    <button class="btn">button</button>
-    <button class="btn btn-primary">Hello pramary</button>
-    <button class="btn btn-secondary">Hello there secondary</button>
+        <p>During my carreer I have used multiple Javascript framewors like:</p>
+        <ul class="frameworks">
+            <li class="vue">
+                <ExternalLink href="https://vuejs.org/" description="Vue JS" />
+            </li>
+            <li class="react">
+                <ExternalLink href="https://react.dev/" description="React" />
+            </li>
+            <li class="svelte">
+                <ExternalLink href="https://svelte.dev/" description="Svelte" />
+            </li>
+            <li class="snap">
+                <ExternalLink href="https://github.com/snap-framework/snap"
+                    description="Snap! (led by the Canada School of Public Service)" />
+            </li>
+        </ul>
+    </div>
+    <div v-else-if="locale === 'fr'">
+        <p>fr</p>
+    </div>
+    <div v-else-if="locale === 'es'">
+        <p>es</p>
+    </div>
+    <div v-else>
+        <p>Content not found</p>
+    </div>
+
+
 </template>
+
+<style>
+ul.frameworks {
+    li {
+        --framework-img: url(../assets/logo.png);
+        list-style-image: var(--framework-img);
+
+        &::marker {
+            font-size: 10px;
+        }
+
+        &.vue {
+            --framework-img: url(../assets/img/frameworks/vuejs.org.png);
+        }
+
+        &.react {
+            --framework-img: url(../assets/img/frameworks/react.dev.ico);
+        }
+
+        &.svelte {
+            --framework-img: url(../assets/img/frameworks/svelte.dev.png);
+        }
+
+        &.snap {
+            --framework-img: url(../assets/img/frameworks/snap.png);
+        }
+    }
+}
+</style>
