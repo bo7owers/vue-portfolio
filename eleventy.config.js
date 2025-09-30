2ad47e619d323cc992ebfe161219d89fda1f71d8
bo7owers <74225602+bo7owers@users.noreply.github.com>
2025-09-30 10:33:04 -0400

    setup ascii doc plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,13 @@
-import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from "@11ty/eleventy";
+import {
+	IdAttributePlugin,
+	InputPathToUrlTransformPlugin,
+	HtmlBasePlugin,
+} from "@11ty/eleventy";
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
-import eleventyAsciidoc from "@asciidoctor/core"
+import eleventyAsciidoc from "eleventy-plugin-asciidoc"
 
 import pluginFilters from "./_config/filters.js";
 
@@ -24,12 +28,14 @@ export default async function (eleventyConfig) {
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig
 		.addPassthroughCopy({
-			"./public/": "/"
+			"./public/": "/",
 		})
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
+	// Watch adoc files
+	eleventyConfig.addWatchTarget("content/**/*.adoc");
 
 	// Watch CSS files
 	eleventyConfig.addWatchTarget("css/**/*.css");
@@ -40,8 +46,6 @@ export default async function (eleventyConfig) {
 	// Bundle <style> content and adds a {% css %} paired shortcode
 	eleventyConfig.addBundle("css", {
 		toFileDirectory: "dist",
-		// Add all <style> content to `css` bundle (use <style eleventy:ignore> to opt-out)
-		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
 		bundleHtmlContentFromSelector: "style",
 	});
 
@@ -49,13 +53,12 @@ export default async function (eleventyConfig) {
 	eleventyConfig.addBundle("js", {
 		toFileDirectory: "dist",
 		// Add all <script> content to the `js` bundle (use <script eleventy:ignore> to opt-out)
-		// Supported selectors: https://www.npmjs.com/package/posthtml-match-helper
 		bundleHtmlContentFromSelector: "script",
 	});
 
 	// Official plugins
 	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-		preAttributes: { tabindex: 0 }
+		preAttributes: { tabindex: 0 },
 	});
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(HtmlBasePlugin);
@@ -68,8 +71,8 @@ export default async function (eleventyConfig) {
 		templateData: {
 			eleventyNavigation: {
 				key: "Feed",
-				order: 4
-			}
+				order: 4,
+			},
 		},
 		collection: {
 			name: "posts",
@@ -81,9 +84,9 @@ export default async function (eleventyConfig) {
 			subtitle: "This is a longer description about your blog.",
 			base: "https://example.com/",
 			author: {
-				name: "Your Name"
-			}
-		}
+				name: "Your Name",
+			},
+		},
 	});
 
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
@@ -99,7 +102,7 @@ export default async function (eleventyConfig) {
 				// e.g. <img loading decoding> assigned on the HTML tag will override these values.
 				loading: "lazy",
 				decoding: "async",
-			}
+			},
 		},
 
 		sharpOptions: {
@@ -107,66 +110,46 @@ export default async function (eleventyConfig) {
 		},
 	});
 
-	// AsciiDoc support: https://www.11ty.dev/docs/plugins/asciidoctor/
-	eleventyConfig.addPlugin(eleventyAsciidoc)
+	// AsciiDoc
+	// eleventyConfig.addPlugin(eleventyAsciidoc, {
+	// 	attributes: {
+	// 		showtitle: true,
+	// 	},
+	// 	safe: "safe",
+	// });
+	//
+	eleventyConfig.addPlugin(eleventyAsciidoc);
+
+	eleventyConfig.addWatchTarget("content/**/*.adoc");
+
+	eleventyConfig.setTemplateFormats("[adoc]")
 
 	// Filters
 	eleventyConfig.addPlugin(pluginFilters);
 
 	eleventyConfig.addPlugin(IdAttributePlugin, {
 		// by default we use Eleventy’s built-in `slugify` filter:
-		// slugify: eleventyConfig.getFilter("slugify"),
-		// selector: "h1,h2,h3,h4,h5,h6", // default
+		slugify: eleventyConfig.getFilter("slugify"),
+		selector: "h1,h2,h3,h4,h5,h6", // default
 	});
 
 	eleventyConfig.addShortcode("currentBuildDate", () => {
-		return (new Date()).toISOString();
+		return new Date().toISOString();
 	});
-
-	// Features to make your build faster (when you need them)
-
-	// If your passthrough copy gets heavy and cumbersome, add this line
-	// to emulate the file copy on the dev server. Learn more:
-	// https://www.11ty.dev/docs/copy/#emulate-passthrough-copy-during-serve
-
-	// eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
-};
+}
 
 export const config = {
 	// Control which files Eleventy will process
-	// e.g.: *.md, *.njk, *.html, *.liquid
-	templateFormats: [
-		"md",
-		"njk",
-		"html",
-		"liquid",
-		"11ty.js",
-	],
+	templateFormats: ["md", "njk", "html", "liquid", "11ty.js", ".adoc", "adoc"],
 
-	// Pre-process *.md files with: (default: `liquid`)
 	markdownTemplateEngine: "njk",
-
-	// Pre-process *.html files with: (default: `liquid`)
 	htmlTemplateEngine: "njk",
 
 	// These are all optional:
 	dir: {
-		input: "content",          // default: "."
-		includes: "../_includes",  // default: "_includes" (`input` relative)
-		data: "../_data",          // default: "_data" (`input` relative)
-		output: "_site"
+		input: "content",
+		includes: "../_includes",
+		data: "../_data",
+		output: "_site",
 	},
-
-	// -----------------------------------------------------------------
-	// Optional items:
-	// -----------------------------------------------------------------
-
-	// If your site deploys to a subdirectory, change `pathPrefix`.
-	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-
-	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-	// it will transform any absolute URLs in your HTML to include this
-	// folder name and does **not** affect where things go in the output folder.
-
-	// pathPrefix: "/",
 };
